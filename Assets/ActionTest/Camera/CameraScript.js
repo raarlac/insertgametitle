3f8aafaeddbf0ec1f5f45584c03c3465588aeb4d
raarlac <raphaellacerda@Raphaels-MacBook-Pro.local>
2014-05-21 23:25:00 -0300

    0.0.1
    Animation/Encounter Test

---
+++ Assets/ActionTest/Camera/CameraScript.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var player:Transform;
+function Start () {
+	
+}
+
+function Update () {
+		transform.position.x=player.position.x;
+		transform.position.y=player.position.y+5;
+		transform.position.z=player.position.z-6;
+}
---
+++ Assets/ActionTest/Player/PlayerScript.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+var speed:float;
+var initialRotation:Quaternion;
+function Start () {
+	initialRotation=transform.rotation;
+}
+
+function Update () {
+	if(onattack)
+	{
+		transform.position.z+=Time.deltaTime;
+		childTransform.LookAt(target);
+		if(transform.position.z>target.position.z)
+		{
+			onattack=false;
+		}
+	}
+	else transform.position.z+=Time.deltaTime*speed;
+}
+
+var onattack:boolean=false;
+var target:Transform;
+var childAnimator:Animator;
+var childTransform:Transform;
+var cameraAnimator:Animator;
+function OnTriggerEnter (other : Collider) 
+{
+	target=other.transform;
+	onattack=true;
+	childAnimator.SetBool("jump",true);
+	cameraAnimator.SetBool("sideview",true);
+}
+
+function OnCollisionEnter(collision : Collision)
+{
+	if(collision.gameObject.tag=="enemy")
+	{
+		Destroy(collision.gameObject);
+		onattack=false;
+		childAnimator.SetBool("jump",false);
+		cameraAnimator.SetBool("sideview",false);
+		childTransform.forward=Vector3(0,0,1);
+	}
+}
